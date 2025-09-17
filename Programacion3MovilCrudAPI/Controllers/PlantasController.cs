38792db2983d886af178870ad67c99c8a5297137
Lautaro Tamborini <lautarotamborini@gmail.com>
2025-09-16 22:51:44 -0300

    Segundo Commit, hice el coso con EF.

---
+++ Programacion3MovilCrudAPI/Controllers/PlantasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.EntityFrameworkCore;
+using Programacion3MovilCrudAPI.Models;
+
+
+namespace Programacion3MovilCrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantasController : ControllerBase
+    {
+    }
+}
